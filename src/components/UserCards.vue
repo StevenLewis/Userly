6d502c2c26d3da4f47c490ed28299122c5f81088
Steve Lewis <steve.lewis@elasticstage.com>
2021-04-19 13:25:56 +0100

    Fixes navigation to footer on mobiles for better UX

--- src/components/UserCards.vue
+++ src/components/UserCards.vue
@@ -4,12 +4,12 @@
         <UserCard v-for="user in users" :key="user.email" :user="user" class="lc-user" :color="color" />
       </ul>
 
-      <nav class="border-t border-gray-200 px-4 flex items-center relative sm:px-0">
+      <nav class="fixed left-0 bottom-0 w-full bg-white border-t border-gray-200 px-4 flex items-center z-20 sm:px-0 sm:relative">
         <a v-if="hasPreviousPage"
            @click.prevent="previousPage"
            alt="Load the previous page of users"
            href="#"
-           class="lc-previous mr-auto border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 md:absolute"
+           class="lc-previous absolute left-0 py-2 px-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700"
         >
           <svg class="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
@@ -17,14 +17,14 @@
           Previous
         </a>
 
-        <div class="hidden md:mx-auto md:-mt-px md:flex">
-          <a v-if="hasPreviousPage" @click.prevent="selectPage(page - 1)" :alt="`Go to page ${page - 1}`" href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
+        <div class="flex mx-auto">
+          <a v-if="hasPreviousPage" @click.prevent="selectPage(page - 1)" :alt="`Go to page ${page - 1}`" href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 py-2 px-4 inline-flex items-center text-sm font-medium">
             {{ page - 1 }}
           </a>
-          <a @click.prevent="selectPage(page)" :alt="`Go to page ${page}`" href="#" class="lc-page border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium" aria-current="page">
+          <a @click.prevent="selectPage(page)" :alt="`Go to page ${page}`" href="#" class="lc-page border-indigo-500 text-indigo-600 border-t-2 py-2 px-4 inline-flex items-center text-sm font-medium" aria-current="page">
             {{ page }}
           </a>
-          <a v-if="hasNextPage" @click.prevent="selectPage(page + 1)" :alt="`Go to page ${page + 1}`" href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
+          <a v-if="hasNextPage" @click.prevent="selectPage(page + 1)" :alt="`Go to page ${page + 1}`" href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 py-2 px-4 inline-flex items-center text-sm font-medium">
             {{ page + 1 }}
           </a>
         </div>
@@ -33,7 +33,7 @@
            @click.prevent="nextPage"
            alt="Load the next page of users"
            href="#"
-           class="lc-next ml-auto right-0 border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 md:absolute"
+           class="lc-next absolute right-0 py-2 px-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700"
         >
           Next
           <svg class="ml-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
